bfc1e285a03155d526c3ece39ccebbfd1ac6a885
Jess Telford <jess+github@jes.st>
2017-08-04 12:43:11 +1000

    node >=v7.6 needed

--- install/004-node.sh
+++ install/004-node.sh
@@ -5,7 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-version=v6.11.1
+version=v7.10.1
 nvm install $version
 nvm alias default $version
 nvm use $version
